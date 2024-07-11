f7c606929237abb5e0e020c6b0022ffa4f86e05e
fkingsh <12200403@inha.edu>
2024-07-11 22:00:00 +0900

    fraction founder

--- SeungHyunKim12200403.py
+++ SeungHyunKim12200403.py
@@ -1,14 +1,23 @@
-# 벌집 : 육각형으로 이루어짐, 중앙을 1번부터 시작해서 바깥을 한 바퀴 돌아가면서 1씩 증가
-# N번 방까지 지나가야하는 최소 방의 갯수 출력
-N = int(input())
-if N-1 == 0:
-    print("1")
+# 분수찾기 : 지그재그로 순서 증가 => 순서 입력받고 순서에 맞는 분수 출력
+# X = sigma n
+# 대각선이 되는 지점을 기준으로 생각해봄 => (2,3) = 2개 / (4,5,6) = 3개 / (7,8,9,10) = 4개 / (11,12,13,14,15) = 5개 / ...
+# 갯수만 정하면 while문을 사용하여 구할 수 있다고 생각함 => sigma n을 받는 변수를 이용해서 n값을 결정
+X = int(input())
+if X == 1:
+    print('1/1')
 else:
-    k = 0
-    share = 1
-    while share <= N-1:
-        share = share + 6*k
-        k += 1
-    print(k)
-# X = 1+sigma(6n) 과 같이 표현 가능하고 n을 구하면 지나가야 하는 최소 방의 갯수를 알 수 있다.
-# 이렇게 조건을 알 때, while을 통하여 편리하게 해결 가능하다.
+    n = 2
+    share = 1 + n
+    while X > share:
+        n += 1
+        share = share + n
+    # 짝수 n => 1/n 부터 시작, 홀수 n => n/1 부터 시작
+    number = share - X
+    if n % 2 == 0:      #짝수
+        print(n-(number),end = '')
+        print("/",end = '')
+        print(n-(n-(number+1)))
+    elif n % 2 == 1:    #홀수
+        print(n-(n-(number+1)),end = '')
+        print("/",end = '')
+        print(n-(number))
