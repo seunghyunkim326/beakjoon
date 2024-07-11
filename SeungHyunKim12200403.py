a5ff1343739a417fd120f51a4ef383419d3e31e4
fkingsh <12200403@inha.edu>
2024-07-11 22:35:56 +0900

    snail want to go up(time over)

--- SeungHyunKim12200403.py
+++ SeungHyunKim12200403.py
@@ -1,23 +1,11 @@
-# 분수찾기 : 지그재그로 순서 증가 => 순서 입력받고 순서에 맞는 분수 출력
-# X = sigma n
-# 대각선이 되는 지점을 기준으로 생각해봄 => (2,3) = 2개 / (4,5,6) = 3개 / (7,8,9,10) = 4개 / (11,12,13,14,15) = 5개 / ...
-# 갯수만 정하면 while문을 사용하여 구할 수 있다고 생각함 => sigma n을 받는 변수를 이용해서 n값을 결정
-X = int(input())
-if X == 1:
-    print('1/1')
-else:
-    n = 2
-    share = 1 + n
-    while X > share:
-        n += 1
-        share = share + n
-    # 짝수 n => 1/n 부터 시작, 홀수 n => n/1 부터 시작
-    number = share - X
-    if n % 2 == 0:      #짝수
-        print(n-(number),end = '')
-        print("/",end = '')
-        print(n-(n-(number+1)))
-    elif n % 2 == 1:    #홀수
-        print(n-(n-(number+1)),end = '')
-        print("/",end = '')
-        print(n-(number))
+# 달팽이는 올라가고 싶다 : A, B, V 입력받음 V미터 까지 올라가는데 걸리는 일 수 구하기
+# 낮에는 A미터 올라감, 밤에는 B미터 내려감
+A, B, V = map(int,input().split())
+count = 0
+while V >= (A-B)*count:
+    count += 1
+    if V <= count*A - (count-1)*B:
+        break
+    else:
+        pass
+print(count)
