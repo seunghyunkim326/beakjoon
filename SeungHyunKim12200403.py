2565f9e22a39d2e4cb400ae8f9ae3096bdd2354c
fkingsh <12200403@inha.edu>
2024-07-12 19:55:58 +0900

    tirangle and 3sides

--- SeungHyunKim12200403.py
+++ SeungHyunKim12200403.py
@@ -1,10 +1,25 @@
-# 달팽이는 올라가고 싶다 : A, B, V 입력받음 V미터 까지 올라가는데 걸리는 일 수 구하기
-# 낮에는 A미터 올라감, 밤에는 B미터 내려감
-A, B, V = map(int,input().split())
-# (V-A)/(A-B) 가 나누어 떨어지는 경우, 나누어 떨어지지 않는 경우 2가지 (후원이가 발견한 규칙)
-# 나누어 떨어지면 +1, 나누어 떨어지지 않으면 +2
-# 정상에 있으면 내려가지 않기 때문에 정상(V)에서 마지막으로 올라가는 경우 A를 빼서 생각한다.
-if (V-A) % (A-B) == 0:
-    print((V-A)//(A-B)+1)
-else:
-    print((V-A)//(A-B)+2)
+# 삼각형과 세 변 : 삼각형 세 변의 길이에 따라 정의
+# Equilateral : 세 변의 길이가 같은 경우, Isosceles : 두 변의 길이만 같은 경우, Scalene : 세 변의 길이가 모두 다른 경우
+# 세 변의 길이가 삼각형 조건을 만족하지 않으면 Invalid 출력
+# 0 0 0 이 입력될 때 까지 입력받음
+while True:
+    array = list(map(int,input().split()))
+    new = sorted(array)
+    if new[0] == new[1] == new[2] == 0:
+        break
+    else:
+        if new[0] + new[1] <= new[2]:
+            print("Invalid")
+        else:
+            if new[0] == new[1]:
+                if new[0] == new[2]:
+                    print("Equilateral")
+                else:
+                    print("Isosceles")
+            elif new[1] == new[2]:
+                if new[0] == new[1]:
+                    print("Equilateral")
+                else:
+                    print("Isosceles")
+            else:
+                print("Scalene")
