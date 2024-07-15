7963e99752e0bb5128b717699185bff6419b9859
fkingsh <12200403@inha.edu>
2024-07-15 17:57:55 +0900

    색종이 미완성

--- SeungHyunKim12200403.py
+++ SeungHyunKim12200403.py
@@ -1,25 +1,15 @@
-# 삼각형과 세 변 : 삼각형 세 변의 길이에 따라 정의
-# Equilateral : 세 변의 길이가 같은 경우, Isosceles : 두 변의 길이만 같은 경우, Scalene : 세 변의 길이가 모두 다른 경우
-# 세 변의 길이가 삼각형 조건을 만족하지 않으면 Invalid 출력
-# 0 0 0 이 입력될 때 까지 입력받음
-while True:
-    array = list(map(int,input().split()))
-    new = sorted(array)
-    if new[0] == new[1] == new[2] == 0:
-        break
-    else:
-        if new[0] + new[1] <= new[2]:
-            print("Invalid")
-        else:
-            if new[0] == new[1]:
-                if new[0] == new[2]:
-                    print("Equilateral")
-                else:
-                    print("Isosceles")
-            elif new[1] == new[2]:
-                if new[0] == new[1]:
-                    print("Equilateral")
-                else:
-                    print("Isosceles")
-            else:
-                print("Scalene")
+# 색종이 : 가로, 세로 100인 정사각형 흰색 도화지에 가로, 세로 10인 정사각형 검은 색종이 붙임
+# 색종이 수 입력 => 색종이 위치 입력 => 검은 부분의 넓이 출력
+# x, y 좌표를 이용하여 색종이가 위치한 부분은 1로 바꿔줌(중복이여도 그대로 1)
+
+N = int(input())
+black = [[0 for _ in range(100)] for _ in range(2)]
+# x(1~100), y(1~100) x,y 좌표 만들기
+
+for _ in range(N):
+    x, y = map(int,input().split())
+    for i in range(10):
+        black[0][x-1+i] += 1
+        black[1][y-1+i] += 1
+        # 검은 부분의 좌표를 1로 표시(완료)
+print(black)
