ee215f8ad3e72f2f67779d730b69e3d5c913fc2e
fkingsh <12200403@inha.edu>
2024-07-15 20:49:48 +0900

    생각이 너무 짧다. 질문 게시판 보고 다시 풀어본다.

--- SeungHyunKim12200403.py
+++ SeungHyunKim12200403.py
@@ -1,15 +1,30 @@
 # 색종이 : 가로, 세로 100인 정사각형 흰색 도화지에 가로, 세로 10인 정사각형 검은 색종이 붙임
 # 색종이 수 입력 => 색종이 위치 입력 => 검은 부분의 넓이 출력
-# x, y 좌표를 이용하여 색종이가 위치한 부분은 1로 바꿔줌(중복이여도 그대로 1)
+# x,y 좌표를 사용하지만 x좌표, y좌표 따로 볼 경우에 겹치지 않아도 2 이상이 되는 문제가 발생한다.
+# 2차원 배열을 이용하여 x좌표, y좌표 두개 전부 고려한다.
 
 N = int(input())
-black = [[0 for _ in range(100)] for _ in range(2)]
-# x(1~100), y(1~100) x,y 좌표 만들기
+black = [[0 for _ in range(2)] for _ in range(N)]
 
-for _ in range(N):
+for i in range(N):
     x, y = map(int,input().split())
-    for i in range(10):
-        black[0][x-1+i] += 1
-        black[1][y-1+i] += 1
-        # 검은 부분의 좌표를 1로 표시(완료)
+    black[i][0] = x
+    black[i][1] = y
+
 print(black)
+
+s = 0
+
+for j in range(N):
+    for k in range(j+1,N-j):
+        x_num = 0
+        y_num = 0
+        if (black[j][0] < black[k][0] < black[j][0]+10) or (black[k][0] < black[j][0] < black[k][0]+10):
+            if (black[j][1] <= black[k][1] < black[j][0]+10) or (black[k][1] < black[j][1] < black[k][1]+10):
+                x_num = 10 - abs(black[k][0]-black[j][0])
+                y_num = 10 - abs(black[k][1]-black[j][1])
+                print(x_num)
+        s = s + (x_num * y_num)
+print(100*N-s)
+# N번째 까지 서로 탐색하는 코드를 만들 때, range(j+1,N-j)를 하면 중복 탐색 없이 서로 한번 씩 탐색 가능하다.
+# 시작 범위도 설정할 수 있음에 주의해라!
