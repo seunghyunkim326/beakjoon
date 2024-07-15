0572862da0e54094bb53f36ef9945b57fd0c90d6
fkingsh <12200403@inha.edu>
2024-07-15 22:02:18 +0900

    질문 게시판을 참고한 방법이지만 (x,y) 좌표 모양에 국한되면 잘 안풀렸다. 리스트의 인덱스를 x,y를 이용하여 쉽게 해결 가능하다.

--- SeungHyunKim12200403.py
+++ SeungHyunKim12200403.py
@@ -2,29 +2,26 @@
 # 색종이 수 입력 => 색종이 위치 입력 => 검은 부분의 넓이 출력
 # x,y 좌표를 사용하지만 x좌표, y좌표 따로 볼 경우에 겹치지 않아도 2 이상이 되는 문제가 발생한다.
 # 2차원 배열을 이용하여 x좌표, y좌표 두개 전부 고려한다.
+# 100*100이라면 한 점이 넓이 1로 생각 가능 => 색종이의 코드에 따라 
 
+result = 0
 N = int(input())
-black = [[0 for _ in range(2)] for _ in range(N)]
+black = [[0 for _ in range(100)] for _ in range(100)]
 
 for i in range(N):
     x, y = map(int,input().split())
-    black[i][0] = x
-    black[i][1] = y
+    for j in range(10):
+        for k in range(10):
+            if black[x+j-1][y+k-1] == 0:
+                black[x+j-1][y+k-1] = 1
 
-print(black)
+for X in range(100):
+    for Y in range(100):
+        if black[X][Y] == 1:
+            result += 1
 
-s = 0
+print(result)
 
-for j in range(N):
-    for k in range(j+1,N-j):
-        x_num = 0
-        y_num = 0
-        if (black[j][0] < black[k][0] < black[j][0]+10) or (black[k][0] < black[j][0] < black[k][0]+10):
-            if (black[j][1] <= black[k][1] < black[j][0]+10) or (black[k][1] < black[j][1] < black[k][1]+10):
-                x_num = 10 - abs(black[k][0]-black[j][0])
-                y_num = 10 - abs(black[k][1]-black[j][1])
-                print(x_num)
-        s = s + (x_num * y_num)
-print(100*N-s)
-# N번째 까지 서로 탐색하는 코드를 만들 때, range(j+1,N-j)를 하면 중복 탐색 없이 서로 한번 씩 탐색 가능하다.
-# 시작 범위도 설정할 수 있음에 주의해라!
+#           
+# 색종이가 있는 좌표에 1표시, 이미 1로 되어있으면 pass
+# for _ in range()
