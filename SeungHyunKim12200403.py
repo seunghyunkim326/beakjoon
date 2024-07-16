fe81b33a5296896819194e2bf0d2b19005e1a244
fkingsh <12200403@inha.edu>
2024-07-16 18:03:31 +0900

    세 막대(sorted 사용 버전)

--- SeungHyunKim12200403.py
+++ SeungHyunKim12200403.py
@@ -1,27 +1,13 @@
-# 색종이 : 가로, 세로 100인 정사각형 흰색 도화지에 가로, 세로 10인 정사각형 검은 색종이 붙임
-# 색종이 수 입력 => 색종이 위치 입력 => 검은 부분의 넓이 출력
-# x,y 좌표를 사용하지만 x좌표, y좌표 따로 볼 경우에 겹치지 않아도 2 이상이 되는 문제가 발생한다.
-# 2차원 배열을 이용하여 x좌표, y좌표 두개 전부 고려한다.
-# 100*100이라면 한 점이 넓이 1로 생각 가능 => 색종이의 코드에 따라 
-
-result = 0
-N = int(input())
-black = [[0 for _ in range(100)] for _ in range(100)]
-
-for i in range(N):
-    x, y = map(int,input().split())
-    for j in range(10):
-        for k in range(10):
-            if black[x+j-1][y+k-1] == 0:
-                black[x+j-1][y+k-1] = 1
-
-for X in range(100):
-    for Y in range(100):
-        if black[X][Y] == 1:
-            result += 1
-
-print(result)
-
-#           
-# 색종이가 있는 좌표에 1표시, 이미 1로 되어있으면 pass
-# for _ in range()
+# 세 막대 : a, b, c 세 막대를 가지고 있음 => 둘레가 최대가 되는 삼각형을 만드는 프로그램
+# 삼각형 조건 : 가장 긴 변 < 나머지 변의 합
+# 가장 큰 변을 찾고 나머지 변의 합을 이용해서 가장 긴 변을 조절하여 합을 출력하면 완성!
+a, b, c = map(int,input().split())
+arr = []
+arr.append(a)
+arr.append(b)
+arr.append(c)
+result = sorted(arr)
+if result[2] >= result[0] + result[1]:
+    print(2 * (result[0] + result[1]) - 1)
+else:
+    print(result[0] + result[1] + result[2])
